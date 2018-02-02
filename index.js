5578db6f563613f9e51d76ecd9c79acfe4f73d6a
Phil Plückthun <phil@plckthn.me>
2018-02-02 13:25:21 -0800

    Fix random gif search

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const { promisify } = require('util')
-
-const giphy = require('giphy-api')({
-  https: true,
-  timeout: 20
-})
-
-const giphyRandom = promisify(giphy.random.bind(giphy))
+const giphy = require('giphy-api')('')
 
 module.exports = (robot) => {
   robot.log('Yay, the app was loaded!')
   robot.on('pull_request.closed', async context => {
-    const gif = await giphyRandom({ q: 'kitten', rating: 'g', fmt: 'json' })
-    const body = `<img src="${gif[0].images.url}" width=400 />`
+    const gif = await giphy.random({ tag: 'kitten', rating: 'g', fmt: 'json' })
+    const body = `<img src="${gif.data.image_url}" width=400 />`
     const params = context.issue({ body })
 
     return context.github.issues.createComment(params)
